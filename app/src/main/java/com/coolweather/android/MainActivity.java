c7bce69272e0b37c6c703bf7296e8d5bb8cad057
zhudong <zhudongdong@91guoxin.com>
2017-08-25 10:34:23 +0800

    配置数据库

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.zhudong.coolweather;
+package com.coolweather.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
